81dedf482a8b63afe7cd1b9a42ead77303958771
Ivan Vladimir Meza Ruiz <ivanvladimir@gmail.com>
2015-10-06 14:17:41 -0500

    Adds options to experiments in NIPS

--- python/nips/experiments.py
+++ python/nips/experiments.py
@@ -9,21 +9,22 @@
 
 import numpy as np
 import pylab as pl
-
 from smh import smh
 
 # MAIN program
 if __name__ == "__main__":
     import argparse
     p = argparse.ArgumentParser("Mines")
-    p.add_argument("-r","--tuple_size",default=4,type=int,
-        action="store", dest='r',help="Size of the tupple")
-    p.add_argument("-l","--number_tuples",default=10,type=int,
-        action="store", dest='l',help="Number of the tupple")
+    p.add_argument("-r","--tuple_size",default=[3],
+                action="append", dest="rs",type=int,
+                help="Size of the tuple")
+    p.add_argument("-l","--number_tuples",default=[120],
+                action="append", dest="ls",type=int,
+                help="Number of tuples")
     p.add_argument("--output",default=None,type=str,
         action="store", dest='output',help="Filename to save mined model")
-    p.add_argument("--min",default=1000,type=int,
-        action="store", dest='min',help="Minimum number of items")
+    p.add_argument("--cutoff",default=None,type=int,
+        action="store", dest='cutoff',help="Cutoff of topics [Off]")
     p.add_argument("--show",default=False,
             action="store_true", dest='show',help="Show bases found")
     p.add_argument("ifs",default=None,
@@ -31,15 +32,45 @@ if __name__ == "__main__":
 
     opts = p.parse_args()
 
+    if len(opts.ls)>1:
+        opts.ls.pop(0)
+    if len(opts.rs)>1:
+        opts.rs.pop(0)
+
     print "Loading file ifs:",opts.ifs
     s=smh.smh_load(opts.ifs)
-    print "Mining topics..."
-    m=s.mine(opts.r,opts.l)
-    print "Size of original mined topics:",m.size()
-    m.cutoff(min=opts.min)
-    print "Size of cutted off mined topics:",m.size()
 
- 
+    pairs=zip(opts.rs,opts.ls)
+    sorted(pairs)
+
+    MASS=[]
+
+    for r,l in pairs:
+        print "Size tuples (r)",r,"Number of tuples (l)",l
+        print "Mining topics..."
+        m=s.mine(r,l)
+        print "Size of original mined topics:",m.size()
+        if opts.cutoff:
+            m.cutoff(min=opts.cutoff)
+        print "Size of cutted off mined topics:",m.size()
+
+        # EVAL
+        lens=[t.size for t in m.ldb]
+        MASS.append(((r,l),lens))
+        
+        # GRAFICAR
+        h=pl.hist(lens)
+        pl.title("r={0},l={1}".format(r,l))
+        pl.show()
+
+    # Draw boxplot sizes
+    ax = pl.subplot(111)
+    pl.boxplot([d for x,d in MASS])
+    pl.xticks(range(1,len(MASS)+1),["r={0},l={1}".format(x[0],x[1]) for x,d in
+        MASS],rotation="vertical")
+    pl.ylim((0,ax.get_ylim()[1]))
+    pl.show()
+
     if opts.output:
         print "Saving resulting model to",opts.output
         smh.smh_save(opts.output)

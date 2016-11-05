56bf3c2a1f21933717652169c182e205e369b6b9
Gibran Fuentes Pineda <gibranfp@unam.mx>
2016-11-04 23:59:11 -0600

    Adds document classification experiments and scripts for generating boxplots from topic files

---
+++ python/smh_document_classification.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+#
+# Gibran Fuentes-Pineda <gibranfp@unam.mx>
+# IIMAS, UNAM
+# 2016
+#
+# -------------------------------------------------------------------------
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# -------------------------------------------------------------------------
+"""
+Performs document classification from document representations created from
+SMH topics.
+"""
+import argparse
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.svm import LinearSVC
+from sklearn.datasets import fetch_20newsgroups
+
+# vocabulary sizes to evaluate
+# vocabulary_sizes = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
+vocabulary_sizes = [100, 200]
+
+def kfold_cv(data, target, k = 10):
+    """
+    Generator for k-fold cross-validation from a given text data and target.
+    Used to evaluate topic discovery methods and SVM classifier.
+    """
+    db_size = len(data)
+    fold_size = int(db_size / 10)
+    for i in range(k):
+        complete_set = range(db_size)
+        valid_set = complete_set[fold_size * i:fold_size * (i + 1)]
+        train_set = [d for d in complete_set if d not in valid_set]
+        
+        data_train = [data[d] for d in train_set]
+        target_train = [target[d] for d in train_set]
+
+        data_valid = [data[d] for d in valid_set]
+        target_valid = [target[d] for d in valid_set]
+
+        yield i, data_train, target_train, data_valid, target_valid
+
+def evaluate_model(model, vectorizer, data, target, k = 10):
+    """
+    Evaluates a topic discovery method in document classification
+    using k-fold cross-validation. Topics are first discovered from
+    the train documents and then all documents are represented using
+    the discovered topics. This representation is used to train and
+    validate a linear svm classifier.
+    """
+    accuracy = np.zeros(k)
+    for i, data_train, target_train, data_valid, target_valid in kfold_cv(data, target, k = 10):
+        X_train = vectorizer.fit_transform(data_train)
+        X_valid = vectorizer.transform(data_valid)
+        
+        model.fit(X_train)
+
+        topic_rep_train = model.transform(X_train)
+        topic_rep_valid = model.transform(X_valid)
+
+        lsvc = LinearSVC()
+        lsvc.fit(topic_rep_train, target_train)
+        accuracy[i] = lsvc.score(topic_rep_valid, target_valid)
+
+    return accuracy.mean()
+
+
+def plot_classification_accuracies(show_flag, path_to_save):
+    """
+    Evaluates NMF, Online LDA and SMH in document classification and plots the performances
+    """
+    print "Loading dataset"
+    dataset = fetch_20newsgroups(subset='all', remove=('headers', 'footers', 'quotes'))
+
+    fig = plt.figure(1, figsize=(9, 6))
+    ax = fig.add_subplot(111)
+
+    print "Evaluating topic discovery methods"
+    for vectorizer,model,name in configurations:
+        print "Evaluating", name
+        accuracy = np.zeros(len(vocabulary_sizes))
+        for i,nf in enumerate(vocabulary_sizes):
+            vectorizer.set_params(max_features=nf)
+            accuracy[i] = evaluate_model(model, vectorizer, dataset.data, dataset.target)
+        ax.plot(vocabulary_sizes, accuracy, label=name)
+
+    ax.legend()
+    plt.ylabel("Accuracy")
+    plt.xlabel("Vocabulary size")
+
+    if show_flag:
+        plt.show()
+
+    if path_to_save:
+        plt.savefig(path_to_save)
+
+def main():
+    try:
+        parser = argparse.ArgumentParser()
+        parser = argparse.ArgumentParser(
+        description="Evaluates SMH in document classification and plots the performances")
+        parser.add_argument('-s', '--show_flag', action='store_true',
+                            help="show figure")
+        parser.set_defaults(fig=False)
+        parser.add_argument("-p", "--path_to_save", type=str, default=None,
+                            help="file where to save the figure")
+        args = parser.parse_args()
+
+        plot_classification_accuracies(args.show_flag, args.path_to_save)
+        
+    except SystemExit:
+        print "for help use --help"
+        sys.exit(2)
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/plots_kbsystems.sh
@@ -0,0 +1,195 @@
+#!/bin/bash
+
+# needs the path of the experiments directory (exps)
+if [ $# -eq 0 ]; then
+      echo "Error: No arguments supplied"
+      exit 1
+fi
+
+# needs the path of the experiments directory (exps)
+if [ ! -d "$1" ]; then
+      echo "Experiments directory $1 doesn't exist"
+      exit 1
+fi
+
+mkdir -p figs
+
+# plot for NIPS (r = 3 and l = [86 118 205 315 692])
+python python/plot_coherences.py \
+       -x "Different values of the parameter \$l\$ (for \$r = 3\$)" \
+       -l "86 118 205 315 692" \
+       -p figs/nips_r3_l.pdf\
+       $1/exp1/topics/nips_1_r_3_l_86.topics\
+       $1/exp1/topics/nips_1_r_3_l_118.topics\
+       $1/exp1/topics/nips_1_r_3_l_205.topics\
+       $1/exp1/topics/nips_1_r_3_l_315.topics\
+       $1/exp1/topics/nips_1_r_3_l_692.topics
+
+# plot for NIPS (r = [2 3 4 5 6] and l = [315])
+python python/plot_coherences.py \
+       -x "Different values of the parameter \$r\$ (for \$l = 315\$)" \
+       -l "2 3 4 5 6" \
+       -p figs/nips_r_l315.pdf\
+       $1/exp1/topics/nips_2_r_2_l_315.topics\
+       $1/exp1/topics/nips_2_r_3_l_315.topics\
+       $1/exp1/topics/nips_2_r_4_l_315.topics\
+       $1/exp1/topics/nips_2_r_5_l_315.topics\
+       $1/exp1/topics/nips_2_r_6_l_315.topics
+
+# plot for NIPS (r = [2 3 4 5 6] and l = [16 86 432 2165 10830])
+python python/plot_coherences.py \
+       -x "Different values of the parameters \$r\$ and \$l\$  (for \$s* = 0.2\$)" \
+       -l "\$r=2,l=16\$ \$r=3,l=86\$ \$r=4,l=432\$ \$r=5,l=2165\$ \$r=6,l=10830\$" \
+       -p figs/nips_r_l_s0.2.pdf\
+       $1/exp1/topics/nips_3_r_2_l_16.topics\
+       $1/exp1/topics/nips_3_r_3_l_86.topics\
+       $1/exp1/topics/nips_3_r_4_l_432.topics\
+       $1/exp1/topics/nips_3_r_5_l_2165.topics\
+       $1/exp1/topics/nips_3_r_6_l_10830.topics
+
+# plot for Reuters (r = 3 and l = [86 118 205 315 692])
+python python/plot_coherences.py \
+       -x "Different values of the parameter \$l\$ (for \$r = 3\$)" \
+       -l "86 118 205 315 692" \
+       -p figs/reuters_r3_l.pdf\
+       $1/exp1/topics/reuters_1_r_3_l_86.topics\
+       $1/exp1/topics/reuters_1_r_3_l_118.topics\
+       $1/exp1/topics/reuters_1_r_3_l_205.topics\
+       $1/exp1/topics/reuters_1_r_3_l_315.topics\
+       $1/exp1/topics/reuters_1_r_3_l_692.topics
+
+# plot for Reuters (r = [2 3 4 5 6] and l = [315])
+python python/plot_coherences.py \
+       -x "Different values of the parameter \$r\$ (for \$l = 315\$)" \
+       -l "2 3 4 5 6" \
+       -p figs/reuters_r_l315.pdf\
+       $1/exp1/topics/reuters_2_r_2_l_315.topics\
+       $1/exp1/topics/reuters_2_r_3_l_315.topics\
+       $1/exp1/topics/reuters_2_r_4_l_315.topics\
+       $1/exp1/topics/reuters_2_r_5_l_315.topics\
+       $1/exp1/topics/reuters_2_r_6_l_315.topics
+
+# plot for Reuters (r = [2 3 4 5 6] and l = [16 86 432 2165 10830])
+python python/plot_coherences.py \
+       -x "Different values of the parameters \$r\$ and \$l\$  (for \$s* = 0.2\$)" \
+       -l "\$r=2,l=16\$ \$r=3,l=86\$ \$r=4,l=432\$ \$r=5,l=2165\$ \$r=6,l=10830\$" \
+       -p figs/reuters_r_l_s0.2.pdf\
+       $1/exp1/topics/reuters_3_r_2_l_16.topics\
+       $1/exp1/topics/reuters_3_r_3_l_86.topics\
+       $1/exp1/topics/reuters_3_r_4_l_432.topics\
+       $1/exp1/topics/reuters_3_r_5_l_2165.topics\
+       $1/exp1/topics/reuters_3_r_6_l_10830.topics
+
+# plot for 20 Newsgroups (r = 3 and l = [86 118 205 315 692])
+python python/plot_coherences.py \
+       -x "Different values of the parameter \$l\$ (for \$r = 3\$)" \
+       -l "86 118 205 315 692" \
+       -p figs/20ng_r3_l.pdf\
+       $1/exp2/topics/20ng_r_3_l_86.topics\
+       $1/exp2/topics/20ng_r_3_l_118.topics\
+       $1/exp2/topics/20ng_r_3_l_205.topics\
+       $1/exp2/topics/20ng_r_3_l_315.topics\
+       $1/exp2/topics/20ng_r_3_l_692.topics
+
+# plot for 20 Newsgroups (r = 3 and l = [86 118 205 315 692]) with vocabulary of 10k
+python python/plot_coherences.py \
+       -x "Different values of the parameter \$l\$ (for \$r = 3\$)" \
+       -l "86 118 205 315 692" \
+       -p figs/20ng10k_r3_l.pdf\
+       $1/exp2/topics/20ng10k_r_3_l_86.topics\
+       $1/exp2/topics/20ng10k_r_3_l_118.topics\
+       $1/exp2/topics/20ng10k_r_3_l_205.topics\
+       $1/exp2/topics/20ng10k_r_3_l_315.topics\
+       $1/exp2/topics/20ng10k_r_3_l_692.topics
+
+# plot for 20 Newsgroups (r = 3 and l = [86 118 205 315 692]) with vocabulary of 5k
+python python/plot_coherences.py \
+       -x "Different values of the parameter \$l\$ (for \$r = 3\$)" \
+       -l "86 118 205 315 692" \
+       -p figs/20ng5k_r3_l.pdf\
+       $1/exp2/topics/20ng5k_r_3_l_86.topics\
+       $1/exp2/topics/20ng5k_r_3_l_118.topics\
+       $1/exp2/topics/20ng5k_r_3_l_205.topics\
+       $1/exp2/topics/20ng5k_r_3_l_315.topics\
+       $1/exp2/topics/20ng5k_r_3_l_692.topics
+
+
+# plot for NIPS (r = 3 and l = [86 118 205 315 692]) with vocabulary of 1k
+python python/plot_coherences.py \
+       -x "Different values of the parameter \$l\$ (for \$r = 3\$)" \
+       -l "86 118 205 315 692 2020 5544" \
+       -p figs/nips1k_r3_l.pdf\
+       $1/exp2/topics/nips1k_r_3_l_86.topics\
+       $1/exp2/topics/nips1k_r_3_l_118.topics\
+       $1/exp2/topics/nips1k_r_3_l_205.topics\
+       $1/exp2/topics/nips1k_r_3_l_315.topics\
+       $1/exp2/topics/nips1k_r_3_l_692.topics\
+       $1/exp2/topics/nips1k_r_3_l_2020.topics\
+       $1/exp2/topics/nips1k_r_3_l_5544.topics
+
+# plot for 20 Newsgroups (r = 3 and l = [86 118 205 315 692])
+python python/plot_coherences.py \
+       -x "Different values of the parameter \$l\$ (for \$r = 3\$)" \
+       -l "86 118 205 315 692" \
+       -p figs/20ng_exp3_r3_l.pdf\
+       $1/exp3/topics/20ng_r_3_l_86.topics\
+       $1/exp3/topics/20ng_r_3_l_118.topics\
+       $1/exp3/topics/20ng_r_3_l_205.topics\
+       $1/exp3/topics/20ng_r_3_l_315.topics\
+       $1/exp3/topics/20ng_r_3_l_692.topics
+
+# plot for 20 Newsgroups (r = 3 and l = [86 118 205 315 692]) with vocabulary of 10k
+python python/plot_coherences.py \
+       -x "Different values of the parameter \$l\$ (for \$r = 3\$)" \
+       -l "86 118 205 315 692" \
+       -p figs/20ng10k_exp3_r3_l.pdf\
+       $1/exp3/topics/20ng10k_r_3_l_86.topics\
+       $1/exp3/topics/20ng10k_r_3_l_118.topics\
+       $1/exp3/topics/20ng10k_r_3_l_205.topics\
+       $1/exp3/topics/20ng10k_r_3_l_315.topics\
+       $1/exp3/topics/20ng10k_r_3_l_692.topics
+
+# plot for 20 Newsgroups (r = 3 and l = [86 118 205 315 692]) with vocabulary of 5k
+python python/plot_coherences.py \
+       -x "Different values of the parameter \$l\$ (for \$r = 3\$)" \
+       -l "86 118 205 315 692" \
+       -p figs/20ng5k_exp3_r3_l.pdf\
+       $1/exp3/topics/20ng5k_r_3_l_86.topics\
+       $1/exp3/topics/20ng5k_r_3_l_118.topics\
+       $1/exp3/topics/20ng5k_r_3_l_205.topics\
+       $1/exp3/topics/20ng5k_r_3_l_315.topics\
+       $1/exp3/topics/20ng5k_r_3_l_692.topics
+
+
+# plot for NIPS (r = 3 and l = [86 118 205 315 692])
+python python/plot_coherences.py \
+       -x "Different values of the parameter \$l\$ (for \$r = 3\$)" \
+       -l "86 118 205 315 692" \
+       -p figs/nips_exp3_r3_l.pdf\
+       $1/exp3/topics/nips_r_3_l_86.topics\
+       $1/exp3/topics/nips_r_3_l_118.topics\
+       $1/exp3/topics/nips_r_3_l_205.topics\
+       $1/exp3/topics/nips_r_3_l_315.topics\
+       $1/exp3/topics/nips_r_3_l_692.topics
+
+# plot for NIPS (r = 3 and l = [86 118 205 315 692]) with vocabulary of 1k
+python python/plot_coherences.py \
+       -x "Different values of the parameter \$l\$ (for \$r = 3\$)" \
+       -l "86 118 205 315 692" \
+       -p figs/nips1k_exp3_r3_l.pdf\
+       $1/exp3/topics/nips1k_r_3_l_86.topics\
+       $1/exp3/topics/nips1k_r_3_l_118.topics\
+       $1/exp3/topics/nips1k_r_3_l_205.topics\
+       $1/exp3/topics/nips1k_r_3_l_315.topics\
+       $1/exp3/topics/nips1k_r_3_l_692.topics
+
+# plot for Reuters (r = 3 and l = [86 118 205 315 692])
+python python/plot_coherences.py \
+       -x "Different values of the parameter \$l\$ (for \$r = 3\$)" \
+       -l "86 118 205 315 692" \
+       -p figs/reuters_exp3_r3_l.pdf\
+       $1/exp3/topics/reuters_r_3_l_86.topics\
+       $1/exp3/topics/reuters_r_3_l_118.topics\
+       $1/exp3/topics/reuters_r_3_l_205.topics\
+       $1/exp3/topics/reuters_r_3_l_315.topics\
+       $1/exp3/topics/reuters_r_3_l_692.topics

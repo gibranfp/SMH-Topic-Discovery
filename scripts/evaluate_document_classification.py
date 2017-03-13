27aae6e95bff1ddb07a6fae167d3450a9fbaa528
Gibran <gibranfp@unam.mx>
2017-03-12 18:58:25 -0600

    Adds scripts to evaluate LDA and SMH topics on document classification

---
+++ scripts/evaluate_document_classification.py
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Script to evaluate LDA and SMH topics on document classification.
+
+##### LDA
+bash scripts/evaluate_lda_document_classification.sh
+
+##### SMH
+bash scripts/evaluate_smh_document_classification.sh
